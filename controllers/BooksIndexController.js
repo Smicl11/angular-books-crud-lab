5e15474ed07263d1b08c8da594ef8dea5790c5e1
smicl11 <bryan.smith094@gmail.com>
2016-04-18 10:50:22 -0700

    complete

--- controllers/BooksIndexController.js
+++ controllers/BooksIndexController.js
@@ -1,7 +1,22 @@
-angular.module('libraryApp')
+angular
+  .module('libraryApp')
   .controller('BooksIndexController', BooksIndexController);
 
 BooksIndexController.$inject=['$http'];
-function BooksIndexController( $http ) {
+
+function BooksIndexController($http) {
   var vm = this;
+
+
+  var apiEndPoint = 'https://super-crud.herokuapp.com/books';
+
+  $http({
+    method: 'GET',
+    url: apiEndPoint
+  }).then(function successCallback(response) {
+    console.log(response.data.books);
+    vm.books = response.data.books;
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
 }
--- controllers/BooksShowController.js
+++ controllers/BooksShowController.js
@@ -1,7 +1,51 @@
 angular.module('libraryApp')
   .controller('BooksIndexController', BooksIndexController);
 
+var apiEndPoint = 'https://super-crud.herokuapp.com/books';
+
 BooksShowController.$inject=['$http', '$routeParams', '$location'];
 function BooksShowController($http, $routeParams, $location) {
   var vm = this;
-};
+  var bookId = $routeParams.id;
+  $http({
+    method: 'GET',
+    url: apiEndPoint+bookId
+  }).then(onBookShowSuccess, onError);
+
+
+  function onBookShowSuccess(response){
+    vm.book = response.data;
+  }
+  function onError(error){
+    console.log('It broken now. Here is error: ', error);
+  }
+
+  vm.updateBook = function(bookToUpdate) {
+    $http({
+      method: 'PUT',
+      url: apiEndPoint + bookToUpdate._id,
+      data: {
+        title : bookToUpdate.title,
+        author : bookToUpdate.author,
+        image : bookToUpdate.image,
+        releaseDate : bookToUpdate.releaseDate
+      }
+    }).then(onBookUpdateSuccess, onError);
+
+    function onBookUpdateSuccess(response){
+      vm.book = response.data;
+      $location.path('/');
+    }
+  };
+
+  vm.deleteBook = function(book) {
+    $http({
+      method: 'DELETE',
+      url: apiEndPoint + book._id,
+    }).then(onBookDeleteSuccess, onError);
+
+    function onBookDeleteSuccess(response){
+      $location.path('/');
+    }
+  };
+}
